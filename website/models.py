3f7cb670a65d9945d560c8d7b80fcf9f2d6319cd
Sidhant Arora <sidhantarora@Sidhants-MacBook-Air.local>
2022-09-28 00:55:46 -0400

    Adding code for login and signup along with SQLlite database

---
+++ website/models.py
@@ -0,0 +1,13 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    gender = db.Column(db.String(20))
+    phone_number = db.Column(db.Integer)
+    password = db.Column(db.String(150))
+    
