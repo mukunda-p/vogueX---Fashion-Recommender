bafba8beb61ee2d9dda6e587ccad02eb51b076b2
Akash Sarda <aksarda@ncsu.edu>
2022-10-06 20:56:00 -0400

    removed pdb

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,9 +19,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_NAME}'.format(DB_NAME=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database.db'))
-    import pdb; pdb.set_trace()
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://fashion:fashion@localhost/fashion_test'
-
     db.init_app(app)
 
     import website.views
