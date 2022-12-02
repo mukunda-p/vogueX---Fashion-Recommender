d56eb588c0bb4313322f4a3f45454fe345778bf9
PranaviSan <46625997+PranaviSan@users.noreply.github.com>
2022-12-02 17:12:37 -0500

    Updated api.py in Weather

--- website/weather/api.py
+++ website/weather/api.py
@@ -1,12 +1,11 @@
 import imp
-
-
 import requests
 
 
 class Config:
     def __init__(self):
-        self.API_KEY = "424ca7dcc9a5422e811220113222311"
+        self.API_KEY1 = "E2GXA3EGPJ7RHYM4MTCH4H7RQ"
+        self.API_KEY2 = "424ca7dcc9a5422e811220113222311"
 
 
 class WeatherAPI:
@@ -14,8 +13,8 @@ class WeatherAPI:
         self.config = Config()
 
     def getCurrentWeather(self, latitude=None, longitude=None, city=None):
-        url = "http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no".format(
-            city=city, API_KEY=self.config.API_KEY
+        url = "http://api.weatherapi.com/v1/current.json?key={API_KEY1}&q={city}&aqi=no".format(
+            city=city, API_KEY1=self.config.API_KEY1
         )
         response = requests.request("GET", url, headers={}, data={})
         if response.status_code != 200:
@@ -28,3 +27,25 @@ class WeatherAPI:
         if "condition" in jsonResponse:
             return jsonResponse["condition"]["text"]
         return ""
+
+    def getfutureWeather(self, date=None, city=None, time=None):
+        url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{City}/{Date}?key={API_KEY2}".format(
+            city=city, Date=date ,API_KEY2=self.config.API_KEY2
+        )
+        response = requests.request("GET", url, headers={}, data={})
+        if response.status_code != 200:
+            raise Exception(
+                "Weather API failed : response code : {code}".format(
+                    code=response.status_code
+                )
+            )
+        jsonResponse = response.json()
+        hours = jsonResponse["days"]["hours"]
+        x = (time.split[":"])[0]
+        index=0
+
+        #hours=24
+        for t in hours :
+            if x==((hours[0].split[":"])[0]) :
+                index=t
+        return hours[index]["conditions"]
