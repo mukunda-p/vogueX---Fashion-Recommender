5e561bc3b9cdd6deb398cfd78ff54f56c2d0f853
kalyan-karnati <karnatik97@gmail.com>
2022-12-03 13:26:27 -0500

    Shopping results

--- website/shopping.py
+++ website/shopping.py
@@ -1,5 +1,18 @@
 import requests
 import json
+from flask_login import login_required, current_user
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    request,
+)
 
 class Shopping:
     def __init__(self):
@@ -10,15 +23,26 @@ class Shopping:
         url = self.url
         payload = {
             "api_key": self.API_KEY ,
-            "image_url": self.image_url,
+            "image_url": image_url,
         }
-        response = requests.post(url, data=payload)
+        response = requests.post(url, data = payload)
         json_response = response.json()
-        data = json_response["data"]
-        result_groups = data["result_groups"][0]
+        res = json_response["data"]
+        result_groups = res["result_groups"][0]
         similar_products = result_groups["similar_products"]
         results=[]
         for product in similar_products:
             if(product["currency"]== "USD"):
                 results.append(product)
         return results
+
+
+shoppingbp = Blueprint("shoppingbp", __name__, url_prefix="/")
+
+@shoppingbp.route("/shopping-results", methods=["POST"])
+# @login_required
+def get_shopping_results():
+    image_url=request.args.get('image_url')
+    s=Shopping()
+    result=s.shopping_results(image_url)
+    return render_template("shopping.html",user=current_user, shopping_results=result,enumerate=enumerate)
