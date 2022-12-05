88204052f01b3c711df677ded2653547ea3a2e40
Mukunda Varma <varma13p@gmail.com>
2022-12-04 19:41:38 -0500

    Fix Query Builder Test case

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -8,10 +8,10 @@ from website.utils import QueryBuilder, WeatherAPI
 
 
 def test_search_image_query_builder(app):
-    query_key_words = ["date night", "first anniversary"]
+    query_key_words = [' gender female', ' in Clear weather to a "Wedding"']
     siObject = QueryBuilder()
     query = siObject.getQueryString(query_key_words)
-    assert query == "suggested dress for date night first anniversary "
+    assert query == 'Suggested Indian outfits for  gender female  in Clear weather to a "Wedding"'
 
 
 def test_weather_api(app):
