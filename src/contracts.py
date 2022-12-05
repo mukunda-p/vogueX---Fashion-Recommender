33b317c3bb114394117ab570ca1b96f552277e68
kalyan-karnati <karnatik97@gmail.com>
2022-12-05 13:00:15 -0500

    Coverage Fix

--- src/contracts.py
+++
@@ -1,8 +0,0 @@
-class RecommendationContractRequest:
-    # RECOMMENDATION PAYLOAD FIELDS
-    OCCASION_KEY = "occasion"
-    CITY_KEY = "city"
-
-
-class RecommendationContractResponse:
-    LINKS = "links"
