d240c123b098d23d6efbcb68e2082f2d788b250d
Smiti Kothari <kotharismiti24@gmail.com>
2024-10-31 09:32:19 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="website",
-    version="1.0.0",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=["flask", "pytest", "flask_testing", "flask_login", "pymysql"],
-)
